4b32301a2cd8b1986c25197c38d0d8e432c91c3d
Noelle <noelle.traff@gmail.com>
2022-10-20 22:11:06 +0300

    todo task part 3

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({extended: false}))
 
 const port = 3001
 
+app.get("/", (req, res) => {
+    res.status(200).send('Nothing on this page! Try /tasks')
+} )
 app.get("/tasks", async function (req, res) {
     try {
         const connection = await mysql.createConnection(config.db)
@@ -24,5 +27,24 @@ app.get("/tasks", async function (req, res) {
     }
 })
 
+app.post("/new", async function(req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        const [result,] = await connection.execute('insert into task (description) values (?) ', [req.body.description])
+        res.status(200).json({id:result.insertId})
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.delete("/delete/:id", async function(req, res) {
+    try{
+        const connection = await mysql.createConnection(config.db)
+        await connection.execute('delete from task where id = ? ',[req.params.id])
+        res.status(200).json({id:req.params.id})
+    } catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
 
 app.listen(port)
